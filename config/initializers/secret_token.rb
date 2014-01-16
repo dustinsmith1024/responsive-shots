ee2a5433f91ff2541421fca861bb870c0f08c7d1
Dustin Smith <dustin.smith@cerner.com>
2014-01-15 19:30:50 -0600

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResponsiveShots::Application.config.secret_key_base = '5e687dae5d91a6b35b28a082ba7b811c6b9ab9d4303a2e4562a2dbc3451c9bb8c5ba8629de7e2c0c2eea55e33752c9c0c6482805cbf3aa6b741c9988747d575f'
