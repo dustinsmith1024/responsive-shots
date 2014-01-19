308ac55199a1d138ed5cf52baff1aa6f23112d37
Dustin Smith <dustin.smith@cerner.com>
2014-01-19 13:35:39 -0600

    removing old prototype code

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -30,43 +30,4 @@ class Screenshot < ActiveRecord::Base
   def delete_file
     File.delete(file) if file
   end
-
-  def run
-    # These might be definable somewhere else?
-    self.error = false
-    Capybara.app_host = url
-    options = {}
-    options[:js_errors]=false
-    Capybara.register_driver :poltergeist do |app|
-        Capybara::Poltergeist::Driver.new(app, options)
-    end
-    Capybara.default_driver = :poltergeist
-    begin
-      visit(url)
-      files = []
-      sizes.each_with_index do |size, index|
-        if size.height && size.width
-          file_name = "screenshot_#{id}_#{index}.png"
-          page.driver.resize_window(size.width, size.height)
-          page.driver.render(file_name, full: true)
-          size.file = file_name
-          files << size.file
-          size.save
-        end
-      end
-    rescue => e
-      puts e
-      self.error = true
-      ScreenshotMailer.screenshot_error_email(self).deliver
-    end # End rescue block
-    
-    # TODO: Could possibly queue this too
-    ScreenshotMailer.screenshot_email(self).deliver unless self.error
-    self.histories.create(email: self.email)
-    self.delivered = Time.now
-    self.save!
-    files.each do |file|
-      File.delete(file)
-    end
-  end
 end
