432d7320492c9fae071c67e2e91ccc3c73ffe3b7
Dustin Smith <dustin.smith@cerner.com>
2014-01-26 22:32:01 -0600

    adding in email for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,17 @@ ResponsiveShots::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
   config.action_mailer.default_url_options = { :host => 'responsiveshots.com' }
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'smith1024.com',
+    :user_name            => 'dds1024@gmail.com',
+    :password             => 'rmxmnrckawncodlz',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+    
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
