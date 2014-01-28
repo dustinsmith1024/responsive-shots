c1e3c2a7b450809f781040b53bd4bb6ab066e55e
Dustin Smith <dustin.smith@cerner.com>
2014-01-27 22:12:16 -0600

    updating subject on emails

--- app/mailers/screenshot_mailer.rb
+++ app/mailers/screenshot_mailer.rb
@@ -1,19 +1,20 @@
 class ScreenshotMailer < ActionMailer::Base
-  default :from => "notifications@smith1024.com"
+  default :from => "notifications@responsiveshots.com"
  
   def screenshot_email(message)
     @message = message
     message.screenshots.each do |screenshot, index|
       attachments[screenshot.file] = File.read(screenshot.file)
     end
+
     mail(:to => @message.email,
-       :subject => "Your screenshot for #{@url}")
+      :subject => "Your screenshot for #{message.url}")
   end
 
   def screenshot_error_email(screenshot)
     @screenshot = screenshot
     
     mail(:to => screenshot.email,
-       :subject => "Errors Taking Your screenshot for #{screenshot.url}")
+      :subject => "Errors Taking Your screenshot for #{screenshot.url}")
   end
 end
